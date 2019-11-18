d1684fa5c8f84b805858003bc39d0082efe4ad1b
Hugo <hugojquinterop@gmail.com>
2019-11-18 06:19:13 -0300

    Django
    Instalacion y configuracion

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\59899\desktop\ap_rest_django\api_rest_django\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
