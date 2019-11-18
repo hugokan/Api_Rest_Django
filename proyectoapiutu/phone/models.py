d2b4f09ebecb28cdb50f03a55777bdf386c0f1f2
Hugo <hugojquinterop@gmail.com>
2019-11-18 06:43:01 -0300

    Models
    Creacion de Models y BD

--- proyectoapiutu/phone/models.py
+++ proyectoapiutu/phone/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Phone(models.Model):
+    name = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    
